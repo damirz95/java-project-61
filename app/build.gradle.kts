ddf11ca5fee798a04c2199757a0789082d9d221c
Damir <turgunbaev.damir@yandex.ru>
2023-12-19 17:13:02 +0500

    add Cli class

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("hexlet.code.App")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}

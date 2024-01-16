ddc225bdcb92359c46c7e7e116a9b1b835b003e7
Damir <turgunbaev.damir@yandex.ru>
2024-01-16 16:08:08 +0500

    rf package

--- Java-Test_PJ/srs/App.java
+++
@@ -1,9 +0,0 @@
-package srs;
-import Brckets.isBracketsBalanced;
-public class App {
-    public static void main(String[] args) {
-        String  brakets = "()()()";
-        boolean result = brakets.isBracketsBalanced();
-        System.out.println(result);
-    }
-}
